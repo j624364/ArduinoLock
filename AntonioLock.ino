1cf9904c4e698503c1b0e5d1081a80bcc1859fb3
j624364 <49117678+j624364@users.noreply.github.com>
2022-03-18 10:08:35 +0000

    Add debug mode

--- AntonioLock.ino
+++ AntonioLock.ino
@@ -10,10 +10,10 @@
 #include <Keypad.h>
 
 char keys[ROW_NUM][COLUMN_NUM] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+	{'1', '2', '3', 'A'},
+	{'4', '5', '6', 'B'},
+	{'7', '8', '9', 'C'},
+	{'*', '0', '#', 'D'}
 };
 
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
@@ -21,34 +21,49 @@ Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_
 static String input_password;
 
 void setup() {
-  Serial.begin(9600);
-  input_password.reserve(32); // maximum input characters is 32, change if needed
-  pinMode(RELAY_PIN, OUTPUT); // initialize pin as an output.
-  digitalWrite(RELAY_PIN, HIGH); // lock the door
+	Serial.begin(9600);
+
+	if constexpr (DebugMode) {
+		Serial.println("Debug mode");
+	}
+	else {
+		input_password.reserve(32); // maximum input characters is 32, change if needed
+		pinMode(RELAY_PIN, OUTPUT); // initialize pin as an output.
+		digitalWrite(RELAY_PIN, HIGH); // lock the door
+	}
 }
 
 void loop() {
-  char key = keypad.getKey();
+	if constexpr (DebugMode) {
+		digitalWrite(RELAY_PIN, HIGH);
+		delay(2000);
+		digitalWrite(RELAY_PIN, LOW);
+		delay(2000);
+	}
+	else {
+		char key = keypad.getKey();
 
-  if (key) {
-    Serial.println(key);
+		if (key) {
+			if constexpr (DebugMode)
+				Serial.println("Key: " + key);
 
-    if (key == '*') {
-      input_password = ""; // reset the input password
-    } else if (key == '#') {
-      if (input_password == password) {
-        Serial.println("The password is correct, unlocking the door in 20 seconds");
-        digitalWrite(RELAY_PIN, LOW);  // unlock the door for 20 seconds
-        delay(20000);
-        digitalWrite(RELAY_PIN, HIGH); // lock the door
-      } else {
-        Serial.println("The password is incorrect, try again");
-      }
+			if (key == '*') {
+				input_password = ""; // reset the input password
+			} else if (key == '#') {
+				if (input_password == password) {
+					Serial.println("Correct password");
+					digitalWrite(RELAY_PIN, LOW);  // unlock the door for 20 seconds
+					delay(WaitTime);
+					digitalWrite(RELAY_PIN, HIGH); // lock the door
+				} else {
+					Serial.println("Incorrect Password");
+				}
 
-      input_password = ""; // reset the input password
-    } else {
-      input_password += key; // append new character to input password string
-    }
-  }
+				input_password = ""; // reset the input password
+			} else {
+				input_password += key; // append new character to input password string
+			}
+		}
+	}
 }
 
--- Config.h
+++ Config.h
@@ -7,5 +7,7 @@ constexpr int COLUMN_NUM = 4; //four columns
 constexpr byte pin_rows[ROW_NUM] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
 constexpr byte pin_column[COLUMN_NUM] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
 
-const char* password = "1234ABC"; // change your password here
+const char* password = "1234"; // change your password here
 
+constexpr bool DebugMode { true };
+constexpr int WaitTime { 20 * 1000 };
